c27f7851090a38951b6251a894dff07b1349e8df
MuhamadRafiBaihaki <91840350+MuhamadRafiBaihaki@users.noreply.github.com>
2022-03-23 22:51:12 +0700

    Update artikel.php

--- Praktikum3/startbootstrap-sb-admin-gh-pages/view/artikel.php
+++ Praktikum3/startbootstrap-sb-admin-gh-pages/view/artikel.php
@@ -2,7 +2,7 @@
     include '../layout/atas.php';
 ?>
 
-<a style="text-decoration: none;" href="" onmouseover="dekor1()" onmouseout="dekor2()" id="home"><h2>Welcome to Muhamad Rafi Baihaki's Page of Praktikum 2</h2></a>
+<a style="text-decoration: none;" href="" onmouseover="dekor1()" onmouseout="dekor2()" id="home"><h2>Welcome to Muhamad Rafi Baihaki's Page of Praktikum 3</h2></a>
     <hr>
 
 <?php
